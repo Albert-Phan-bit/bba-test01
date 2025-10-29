a27309c4825b095ecce7da9209fbbb98e9aab4fd
test 01 - Phan Truong Quoc <ptruongquoc97@gmail.com>
2025-10-30 01:06:46 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,14 @@
+let powerUp = "mushroom";
+let effect = '';
+
+if (powerUp == "mushroom") {
+    effect = "Mario be come Super!";
+} else if (powerUp == "flower") {
+    effect = "Mario can shoot fireballs!";
+} else if (powerUp == "star") {
+    effect = "Mario is invincible!";
+} else if (powerUp == "none") {
+    effect = "Mario is normal!";
+}
+
+console.log(effect);
